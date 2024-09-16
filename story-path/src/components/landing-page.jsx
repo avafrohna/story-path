f1a5491767d91845f30106c4427b94d119c24157
Ava Frohna <122284870+avafrohna@users.noreply.github.com>
2024-09-16 15:52:23 +1000

    can't figure out css

--- story-path/src/components/landing-page.jsx
+++ story-path/src/components/landing-page.jsx
@@ -2,30 +2,30 @@ import { Link } from 'react-router-dom';
 
 function LandingPage() {
     return (
-      <div>
+      <div className="landing-page">
         <h1>Landing Page</h1>
-        <text>
-            Hello, welcome to story path!
-        </text>
-        <div>
-        <Link to="/list-projects">
-          <button>View Projects</button>
-        </Link>
-        <Link to="/add-project">
-          <button>Add New Project</button>
-        </Link>
-        <Link to="/list-locations">
-          <button>View Locations</button>
-        </Link>
-        <Link to="/add-location">
-          <button>Add New Location</button>
-        </Link>
-        <Link to="/preview">
-          <button>Preview</button>
-        </Link>
-      </div>
+        <p>Welcome to the StoryPath App!</p>
+
+        <div className="button-container">
+            <Link to="/list-projects">
+              <button>View Projects</button>
+            </Link>
+            <Link to="/add-project">
+              <button>Add New Project</button>
+            </Link>
+            <Link to="/list-locations">
+              <button>View Locations</button>
+            </Link>
+            <Link to="/add-location">
+              <button>Add New Location</button>
+            </Link>
+            <Link to="/preview">
+              <button>Preview</button>
+            </Link>
+        </div>
       </div>
     );
 }
   
 export default LandingPage;
+  
